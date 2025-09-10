441ed6c5566242916e3e98a2fa35f831ecc5f6bc
AMarsh00 <56977848+AMarsh00@users.noreply.github.com>
2025-09-10 11:28:02 -0600

    Update Setup.py

--- Source Code/Setup.py
+++ Source Code/Setup.py
@@ -5,11 +5,18 @@ Last Edit 10 September 2025
 
 GNU Affero General Public License
 
-Python file that builds the c++ LibTorch module into a .so PyTorch module for Python 3.13.
+Python file that builds the c++ LibTorch module into a .so PyTorch module.
 """
 
 from setuptools import setup
 from torch.utils.cpp_extension import CppExtension, BuildExtension
+import sys
+
+# Detect platform-specific compiler flags
+if sys.platform == 'win64':
+    extra_compile_args = ['/O2', '/std:c++17']  # MSVC optimization + C++17
+else:
+    extra_compile_args = ['-O3', '-std=c++17']  # GCC/Clang optimization + C++17
 
 setup(
     name='LooseTopologicalNode',
