bd6b97d4fdc84e432734542dd3b118f9f4ffb915
AMarsh00 <56977848+AMarsh00@users.noreply.github.com>
2025-09-10 11:39:47 -0600

    Update Bindings.cpp

--- Source Code/Bindings.cpp
+++ Source Code/Bindings.cpp
@@ -9,7 +9,7 @@
 * This works by allowing us to compile the module as a python .so file, which we can import in PyTorch.
 */
 
-include <pybind11/pybind11.h>
+#include <pybind11/pybind11.h>
 #include <torch/extension.h>
 #include "TopologicalNodes.h"
 
@@ -26,7 +26,7 @@ PYBIND11_MODULE(LooseTopologicalNode, m) {
         .def("forward", &LooseTopologicalNodeImpl::forward, "Run forward pass through encoder, torus projection, and decoder");
 
     // Bind the module holder (wrapper for use like a standard torch.nn module in Python)
-    py::class_<LooseTopologicalNode, torch::nn::ModuleHolder<LooseTopologicalNodeImpl>>(m, "LooseTopologicalNode")
+    py::class_<LooseTopologicalNode, std::shared_ptr<LooseTopologicalNode>>(m, "LooseTopologicalNode")
         .def(py::init<int, int>(), "Simplified constructor (input_dim, output_dim)")
         .def(py::init<float, int, int, int, int>(), "Full constructor (leak_factor, input_dim, output_dim, num_hidden_layers, hidden_layer_size)")
         .def("forward", [](LooseTopologicalNode& self, torch::Tensor x) {
